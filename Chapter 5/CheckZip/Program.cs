1df04cd0a8fb5792f8199f8062aecbbde1e2eb13
spencerj <vorpehd@gmail.com>
2014-09-22 08:40:16 -0400

    Chap 5
    ........

---
+++ Chapter 5/CheckZip/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CheckZip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] zip = { 12985, 12746, 46573, 15360, 22647, 35065, 74635, 74537, 64058, 64420 };
+            for (int i = 0; i < zip.Length; i++)
+            {
+                Console.Write("Enter A Zip Code: ");
+                int input = Convert.ToInt32(Console.ReadLine());
+
+                if (input == zip[i])
+                {
+                    Console.WriteLine("We Deliver There (:");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("We Dont Deliver There ):");
+                    break;
+                }
+            }
+
+        }
+    }
+}
---
+++ Chapter 5/IntegerList/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IntegerList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] l = { 1, 2, 3, 4, 5 };
+            Console.WriteLine(l[0]);
+            Console.WriteLine(l[1]);
+            Console.WriteLine(l[2]);
+            Console.WriteLine(l[3]);
+            Console.WriteLine(l[4]);
+            Console.WriteLine(".");
+            Console.WriteLine(l[4]);
+            Console.WriteLine(l[3]);
+            Console.WriteLine(l[2]);
+            Console.WriteLine(l[1]);
+            Console.WriteLine(l[0]);
+        }
+    }
+}
